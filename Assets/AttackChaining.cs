0283bcf90efaf2987ef269d3c3bdaeff6aa3ae64
Unfulfilled <btm7428@gmail.com>
2023-04-28 00:02:36 -0400

    Light Attacks
    Fixed Light attack timer
    
    Added max chain limit
    
    pooped on floor

--- Assets/AttackChaining.cs
+++ Assets/AttackChaining.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEditor.Animations;
 using UnityEngine;
+using System;
 
 public class AttackChaining : MonoBehaviour
 {
@@ -8,34 +9,36 @@ public class AttackChaining : MonoBehaviour
 
     public float attackDelay = 0.5f;
     public bool attackReady = true;
-    private float attackTimer = 0.0f;
+    [SerializeField] private float _attackTimer = 0.0f;
 
     public float chainTimeFrame = 1.5f;
-    private float chainTimer = 0.0f;
-    private int chainCounter = 0;
+    public int _chainCounter = 0;
+    private const int _maxChain = 5;
+    [SerializeField] private float _chainTimer = 0.0f;
 
 
     private void Update()
     {
-        if(attackTimer < attackDelay)
+        if(_attackTimer < attackDelay)
         {
-            attackTimer += Time.deltaTime;
+            _attackTimer += Time.deltaTime;
         }
         else
         {
             attackReady = true;
         }
 
-        if(chainCounter > 0)
+        if(_chainCounter > 0)
         {
-            if (chainTimer < chainTimeFrame)
+            if (_chainTimer < chainTimeFrame)
             {
-                chainTimer += Time.deltaTime;
+                _chainTimer += Time.deltaTime;
             }
             else
             {
-                chainTimer = 0.0f;
-                chainCounter = 0;
+                _chainTimer = 0.0f;
+                _chainCounter = 0;
+                displayText.text = "Idle";
             }
         }
 
@@ -54,20 +57,25 @@ public class AttackChaining : MonoBehaviour
     {
         if(!attackReady) { return; }
 
-        Debug.Log(chainCounter);
+        if(_chainCounter == _maxChain)
+        {
+            _chainCounter = 0;
+        }
 
-        if(chainCounter == 0) { displayText.text = "Light Attack"; }
+        Debug.Log(_chainCounter);
+
+        if(_chainCounter == 0) { displayText.text = "Light Attack"; }
         else
         {
-            displayText.text = $"Light Attack {chainCounter}";
-            chainTimer = 0.0f;
+            displayText.text = $"Light Attack {_chainCounter}";
+            _chainTimer = 0.0f;
         }
 
         // Reset the attack ready state
         attackReady = false;
-        attackTimer = 0.0f;
+        _attackTimer = 0.0f;
 
-        chainCounter++;
+        _chainCounter++;
     }
 
     void HeavyAttack()
